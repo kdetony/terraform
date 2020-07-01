93fb11194cbd2220097b0b97a914b4b5e8a7fd7e
Anthony Mogrovejo <kdetony@kdetony.com>
2020-07-01 14:56:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,8 @@ provisioner  "remote-exec" {
     "systemctl disable firewalld",
     "curl -fsSL https://get.docker.com -o get-docker.sh",
     "sh get-docker.sh",
-    "systemctl enable docker" 
+    "systemctl enable docker",
+    "systemctl start docker" 
           ]
  }
 }
